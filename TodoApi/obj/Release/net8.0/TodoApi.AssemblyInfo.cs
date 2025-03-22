67a5dc99d372265e5f82fe18f412cc72e1ce7329
Brachi <brachi2809@gmail.com>
2025-03-23 00:46:53 +0200

    doker

--- TodoApi/obj/Release/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Release/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d749e0db46aac640747a44d44fa3db094f33c6ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a631e72c5cda0a54c31a349f4e338f9195733470")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
