3af897026bbab055650fcfea1cc36ec6ccb6ed8e
Brachi <brachi2809@gmail.com>
2025-03-25 19:46:23 +0200

    מעודכן1

--- client/src/service.js
+++ client/src/service.js
@@ -3,13 +3,13 @@ axios.defaults.baseURL = "http://localhost:5073";
 // axios.defaults.baseURL = "https://client-7stx.onrender.com/";
 
 
-// axios.interceptors.response.use(
-//   response => response,
-//   error => {
-//     console.error("Error in API call:", error.response?.status, error.message);
-//     return Promise.reject(error);
-//   }
-// );
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    console.error("Error in API call:", error.response?.status, error.message);
+    return Promise.reject(error);
+  }
+);
 export default {
   getTasks: async () => {
     const result = await axios.get(`/items`)    
