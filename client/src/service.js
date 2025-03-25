97f4bcc0b177b30ad93aade4242ed58351923fec
Brachi <brachi2809@gmail.com>
2025-03-25 19:15:46 +0200

    bbb

--- client/src/service.js
+++ client/src/service.js
@@ -1,47 +1,3 @@
-// import axios from 'axios';
-// axios.defaults.baseURL = "http://localhost:5073";
-
-// axios.interceptors.response.use(
-//   response => response,
-//   error => {
-//     console.error("Error in API call:", error.response?.status, error.message);
-//     return Promise.reject(error);
-//   }
-// );
-// export default {
-//   getTasks: async () => {
-//     const result = await axios.get(`/items`)    
-//     return result.data;
-//   },
-
-//   addTask: async(name)=>{
-//     console.log('addTask', name);
-//     const result = await axios.post(`/items`, {name: name});
-//     console.log("add");
-    
-//     console.log("add result.data :",result.data);
-
-//     return result.data;
-//   },
-
-//   setCompleted: async(id, isComplete)=>{
-//     console.log('setCompleted', {id, isComplete})
-//     isComplete === true ? isComplete = 1 : isComplete = 0;
-//     const result = await axios.put(`/items/${id}`, {  isComplete });
-//     console.log("set result.data :",result.data);
-//     return result.data;
-//   },
-
-//   deleteTask:async(id)=>{
-//     console.log('deleteTask')
-//     const result = await axios.delete(`/items/${id}`);
-//     console.log("delete result.data :",result.data);
-    
-//     return result.data;
-//   }
-// };
-
-
 import axios from 'axios';
 axios.defaults.baseURL = "http://localhost:5073";
 
@@ -60,7 +16,7 @@ export default {
 
   addTask: async(name)=>{
     console.log('addTask', name);
-    const result = await axios.put(`/items`, {name: name});
+    const result = await axios.post(`/items`, {name: name});
     console.log("add");
     
     console.log("add result.data :",result.data);
@@ -71,7 +27,7 @@ export default {
   setCompleted: async(id, isComplete)=>{
     console.log('setCompleted', {id, isComplete})
     isComplete === true ? isComplete = 1 : isComplete = 0;
-    const result = await axios.post(`/items/${id}`, {  isComplete });
+    const result = await axios.put(`/items/${id}`, {  isComplete });
     console.log("set result.data :",result.data);
     return result.data;
   },
@@ -84,3 +40,4 @@ export default {
     return result.data;
   }
 };
+
