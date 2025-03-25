ec76a3db5ca7db083964312ecb95babe9fb68a2c
Brachi <brachi2809@gmail.com>
2025-03-25 19:50:58 +0200

    מעודכן1

--- client/src/service.js
+++ client/src/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-axios.defaults.baseURL = "http://localhost:5073";
-// axios.defaults.baseURL = "https://client-7stx.onrender.com/";
+// axios.defaults.baseURL = "http://localhost:5073";
+axios.defaults.baseURL = "https://client-7stx.onrender.com/";
 
 
 axios.interceptors.response.use(
