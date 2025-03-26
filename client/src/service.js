4300315488009d17fb16a29bc6caffdd9f2135c8
Brachi <brachi2809@gmail.com>
2025-03-26 21:37:05 +0200

    03

--- client/src/service.js
+++ client/src/service.js
@@ -30,16 +30,13 @@ export default {
   setCompleted: async (id, isComplete, name) => {
     try {
       const taskData = { Name: name, IsComplete: isComplete };
-      const result = await axios.put(`/items/${id}`, taskData);
-  
-      console.log("Response from API:", result.data); // הדפסת התשובה לבדיקה
-  
-      return result.data; 
+      const data = await axios.put(`/items/${id}`, taskData);  
+      return data.data; 
     } catch (error) {
       console.error("Error updating task:", error.response ? error.response.data : error);
       throw error; // נזרוק את השגיאה כדי שהפונקציה הקוראת תוכל לטפל בה
     }
-  }
+  },
   
   
   
