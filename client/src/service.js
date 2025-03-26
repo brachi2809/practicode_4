127bae6b6edfb106dee6e2a92981b6f318cba9c7
Brachi <brachi2809@gmail.com>
2025-03-26 21:24:22 +0200

    01

--- client/src/service.js
+++ client/src/service.js
@@ -22,17 +22,17 @@ export default {
     return result.data;
   },
 
-  // setCompleted: async (id, isComplete) => {
-  //   const result = await axios.put(`/items/${id}`, { isComplete });  // עדכון רק סטטוס
-  //   return result.data; // מחזיר את המשימה המעודכנת
+  setCompleted: async (id, isComplete) => {
+    const result = await axios.put(`/items/${id}`, { isComplete });  // עדכון רק סטטוס
+    return result.data; // מחזיר את המשימה המעודכנת
+  },
+  // setCompleted: async (id, isComplete, name) => {
+  //   const taskData = { Name: name, IsComplete: isComplete }; // ודא שאתה שולח את שם המשימה ואת הסטטוס
+  //   const result = await axios.put(`/items/${id}`, taskData);
+  //   return result.data;
   // },
-  setCompleted: async (id, isComplete, name) => {
-    const taskData = { Name: name, IsComplete: isComplete }; // ודא שאתה שולח את שם המשימה ואת הסטטוס
-    const result = await axios.put(`/items/${id}`, taskData);
-    return result.data;
-  }
   
-,  
+  
 
   deleteTask: async (id) => {
     const result = await axios.delete(`/items/${id}`);
